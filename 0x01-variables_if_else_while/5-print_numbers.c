ab8223e295cf84f14995ae84d2f8196d50a53120
Great Okotie <greatovieokotie@gmail.com>
2022-06-03 14:05:17 -0700

    print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all digit
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	
+	print("\n");
+	return (0);
+}
