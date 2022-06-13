fed625a816a679308529ea4ec995e86d00ca9fcc
Great Okotie <greatovieokotie@gmail.com>
2022-06-13 04:28:43 -0700

    print

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,8 @@
 /**
  * print_alphabet - Prints the alphabet in lowercase
  *
- * Return: Always 0 (Success)
+ * Return: Alwas 0 (Success)
  */
-
 void print_alphabet(void)
 {
 	char alpha;
@@ -14,5 +13,6 @@ void print_alphabet(void)
 	{
 		_putchar (alpha);
 	}
-	_putchar('\n');
+	_putchar('\n')
 }
+
